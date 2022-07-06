a8b833a3c0d9b5d9fc641e58ee8bde84378c0034
anujpatel7181 <102283105+anujpatel7181@users.noreply.github.com>
2022-07-06 10:43:02 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Forecast from "./Components/Forecast";
 import Inputs from "./Components/Inputs";
+import TemparatureAndDetails from "./Components/TemparatureAndDetails";
+import TimeAndLocation from "./Components/TimeAndLocation";
 import TopButtons from "./Components/TopButtons";
+import getFormattedWeatherData from "./Services/weatherServices";
 // import Uilreact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
+  const [query, setQuery] = useState({ q: "Ghatlodiya" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} setUnits={setUnits} units={units} />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemparatureAndDetails weather={weather} />
+          <Forecast title={"hourly forecast"} items={weather.hourly} />
+          <Forecast title={"daily forecast"} items={weather.daily} />
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-const Inputs = () => {
+const Inputs = ({ setQuery, setUnits, units }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearchClick = () => {
+    if (city !== "") setQuery({ q: city });
+  };
+
+  const handleKeyDownSearch = (e) => {
+    if (e.key === "Enter") setQuery({ q: city });
+  };
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({ lat, lon });
+      });
+    }
+  };
+
+  const handleUnitSearch = (e) => {
+    const selectedUnit = e.currentTarget.name;
+
+    if (units !== selectedUnit) setUnits(selectedUnit);
+  };
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search...."
           className="text-xl font-light p-2 focus:outline-none w-full shadow-xl capitalize"
+          onKeyDown={handleKeyDownSearch}
         />
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilLocationPoint
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
         />
       </div>
 
@@ -24,6 +56,7 @@ const Inputs = () => {
         <button
           name="metric"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitSearch}
         >
           °C
         </button>
@@ -31,6 +64,7 @@ const Inputs = () => {
         <button
           name="imperial"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitSearch}
         >
           °F
         </button>
